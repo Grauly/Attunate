121cd43d8c35f653417d60c67cb2fe3e27346bfe
Grauly <nils.dellemann@web.de>
2024-09-22 00:36:39 +0200

    feat: added basic beam shot test
    No, it does not work yet

--- src/main/kotlin/grauly/attunate/Attunate.kt
+++ src/main/kotlin/grauly/attunate/Attunate.kt
@@ -1,16 +1,22 @@
 package grauly.attunate
 
+import grauly.attunate.item.WardensEchoItem
 import net.fabricmc.api.ModInitializer
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
 object Attunate : ModInitializer {
 	public const val MODID : String = "attunate"
     public val LOGGER= LoggerFactory.getLogger(MODID)
+	val WARDENS_ECHO = WardensEchoItem()
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		LOGGER.info("Hello Fabric world!")
+		Registry.register(Registries.ITEM, Identifier(MODID, "wardens_echo"), WARDENS_ECHO)
 	}
 }
--- src/main/kotlin/grauly/attunate/item/WardensEchoItem.kt
+++ src/main/kotlin/grauly/attunate/item/WardensEchoItem.kt
@@ -6,18 +6,35 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
+import net.minecraft.world.RaycastContext
 import net.minecraft.world.World
+import java.awt.Color
 
 class WardensEchoItem : Item(
     FabricItemSettings()
 ) {
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
         val stack = user.getStackInHand(hand)
-        if(world.isClient()) return TypedActionResult.fail(stack)
+        //haha, funny kotlin "smart" cast
+        if (user.itemCooldownManager.isCoolingDown(stack.item)) return TypedActionResult.fail(stack)
+        if (world.isClient() || world !is ServerWorld) return TypedActionResult.fail(stack)
         //now I am on server :)
         Attunate.LOGGER.info("Hello from server!")
+        val hit = world.raycast(
+            RaycastContext(
+                user.eyePos,
+                user.rotationVector.multiply(50.0),
+                RaycastContext.ShapeType.VISUAL,
+                RaycastContext.FluidHandling.ANY,
+                user
+            )
+        )
+        val hitPos = hit?.pos ?: user.rotationVector.multiply(50.0)
+        ShotBeamPacket.sendShotToAll(user.eyePos, hitPos, Color(0, 100, 125), world)
+        user.itemCooldownManager.set(stack.item, 10)
         return super.use(world, user, hand)
     }
 }
